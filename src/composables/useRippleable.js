47be280e8ba2f766b2e63a8bb5e83188c7abc122
Edge Broker GmbH <48259303+edgebroker@users.noreply.github.com>
2025-09-15 12:38:39 +0200

    feat: support ripple options

---
+++ src/composables/useRippleable.js
@@ -0,0 +1,35 @@
+import { h, withDirectives } from 'vue'
+import Ripple from '../../packages/vuetify/src/directives/ripple'
+
+export const rippleableProps = {
+  ripple: {
+    type: [Boolean, Object],
+    default: true,
+  },
+}
+
+export default function useRippleable (props, { onChange = () => {} } = {}) {
+  function genRipple (data = {}) {
+    if (!props.ripple) return null
+
+    const {
+      class: className,
+      directives = [],
+      ...rest
+    } = data
+
+    const node = h('div', {
+      ...rest,
+      class: ['v-input--selection-controls__ripple', className],
+      onClick: onChange,
+    })
+
+    return withDirectives(node, [
+      ...directives,
+      [Ripple, typeof props.ripple === 'object' ? props.ripple : { center: true }],
+    ])
+  }
+
+  return { genRipple }
+}
+
